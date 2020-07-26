c33620792e555457dd95702a164fe64f3566738e
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-26 13:43:55 -0700

    changes to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,19 @@
 import React from 'react'; 
-// import coverImage from 
+// import myImage from assets
+import myImage from '../../assets/images/myimage.jpeg'
 
 function About() {
     return(
-    <section className="about">
-      <h1 class="aboutme">Nicole Wallace</h1>
+    <section className="title">
+      <h1 class="name">Nicole Wallace</h1>
       {/* <img src={coverImage} className="banner" alt="cover" /> */}
-
-      <h2 class="welcome">About Me</h2>
+      
+      <div className="row" id="about-container">
+      <div className="myImage" id="myImage">
+          <img src={myImage} className="my-image" alt="nicole-profile"/>
+      </div>
+      <div className="col-8">
+      <h2 class="welcome">GET TO KNOW ME</h2>
       <p>
       I switched my gears from my education in Business to take on Web Development to keep challenging myself in ways I thought I never could. 
       Web Development allows me to continually create and creatively find new ways to make processes better. I always believed that anything can be improved and 
@@ -18,6 +24,8 @@ function About() {
       with 3 different Liberal Arts Associate’s Degrees. I am a naturally curious person which makes me want to always learn new things. I am a problem-solver who 
       questions everything. I am a hard worker who doesn't take life too seriously and enjoys the little things. But most importantly, I love to have a good laugh.
       </p>
+      </div>  
+      </div>
     </section> 
 )}
   
