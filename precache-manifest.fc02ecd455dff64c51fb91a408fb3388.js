c8a65cd8f6160f87275d83f2170366da40f9ff79
nicolewallace09 <nicole.elisaw@gmail.com>
2020-07-26 21:55:09 -0700

    Updates

---
+++ precache-manifest.fc02ecd455dff64c51fb91a408fb3388.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691ef462ac681bba91c0b139fe949684",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9de42a125e91c70b2a55",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fbe5888a53a324b598c",
+    "url": "/react-portfolio/static/css/main.75dc1e35.chunk.css"
+  },
+  {
+    "revision": "9de42a125e91c70b2a55",
+    "url": "/react-portfolio/static/js/2.98b82c6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.98b82c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbe5888a53a324b598c",
+    "url": "/react-portfolio/static/js/main.28c797d3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "c423121f1d80ba5e82ef8cfcf74af5bf",
+    "url": "/react-portfolio/static/media/myimage.c423121f.jpeg"
+  }
+]);
